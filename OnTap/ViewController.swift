424f52e6cb3b1b3a5b4399ad42ed1491aa6f8aef
Nick Martinson <nick-martinson@uiowa.edu>
2014-12-22 23:19:57 -0600

    Initial Commit

---
+++ OnTap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnTap
+//
+//  Created by Nick Martinson on 12/22/14.
+//  Copyright (c) 2014 Nick Martinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
